9cf829c61509a14b0c11c3cefb92648b0cb42d11
priyans07 <100975770+priyans07@users.noreply.github.com>
2023-03-03 20:56:34 +0530

    Add files via upload

---
+++ random colour generator/app.js
@@ -0,0 +1,10 @@
+const getColor=()=>{
+    const randomNumber=Math.floor(Math.random()*1677215);
+    const randomCode= "#" +randomNumber.toString(16);
+    document.body.style.backgroundColor=randomCode;
+    document.getElementById("color-code").innerHTML=randomCode
+    // console.log(randomNumber, randomCode);
+    navigator.clipboard.writeText(randomCode)
+}
+document.getElementById("btn").addEventListener("click",getColor)
+getColor();
